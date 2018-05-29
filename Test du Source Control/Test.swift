306647579ae2b1de2bdbed32a879cb45d4ce319c
Snoopy <snoopy@macbook-pro-de-sergent.local>
2018-05-29 13:05:42 +0200

    Ajout class Test

---
+++ Test du Source Control/Test.swift
@@ -0,0 +1,16 @@
+//
+//  Test.swift
+//  Test du Source Control
+//
+//  Created by Snoopy on 29/05/2018.
+//  Copyright © 2018 EMANET. All rights reserved.
+//
+
+import Foundation
+
+class Test {
+    
+    func salut() {
+        print("Salut les codabei...")
+    }
+}
